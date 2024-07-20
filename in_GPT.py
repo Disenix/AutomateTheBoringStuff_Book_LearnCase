7c6098b2c5285d4e6d2ef2c5745af5804a5f0dd5
Anton Haponenka <lite.light.awes@gmail.com>
2024-07-20 12:14:05 +0200

    Add files via upload

---
+++ in_GPT.py
@@ -0,0 +1,4 @@
+myString = 'Hello World'
+print('H' in myString) #True
+print('z' in myString) #False
+print('' in myString)
---
+++ myPets.py
@@ -0,0 +1,28 @@
+#myPets = ['Zophie', 'Pooka', 'Fat-tail']
+#print('Enter a pet name:')
+#name = input()
+#if name not in myPets:
+#    print('I do not have a pet named ' + name)
+#else:
+#    print(name + ' is my pet.')
+
+
+#my_string = "hello"
+#for index, char in enumerate(my_string):
+#    print(f"Индекс: {index}, Символ: {char}")
+
+#>>>import random
+#>>>pets = ['Dog', 'Cat', 'Moose']
+#>>>random.choice(pets)
+#>>>random.choice(pets)
+#>>>random.choice(pets)
+
+# random.choice(somelist) - короткая форма someList[random.randint(0, len(someList) - 1]
+
+
+#>>>import random
+#>>>people = ['Alice', 'Bob', 'Carol', 'David']
+#>>>random.shuffle(people)
+#>>>people
+
+
